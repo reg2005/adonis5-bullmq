15141898276f75d3b2674884f79413593aae767d
Evgeniy <cmp08@ya.ru>
2021-02-09 22:12:10 +0300

    fix: ioc typings

--- adonis-typings/index.ts
+++ adonis-typings/index.ts
@@ -1,6 +1,22 @@
 declare module '@ioc:Adonis/Addons/BullMQ' {
-  export { ConnectionOptions as BullMQConfig } from 'bullmq'
-  import { BullMQClass } from 'src/BullMQ'
-  const bullMQClass: BullMQClass
-  export default bullMQClass
+  import Config from '@ioc:Adonis/Core/Config'
+  import {
+    ConnectionOptions as BullMQConfig,
+    Processor,
+    Worker as BullWorker,
+    Queue as BullQueue,
+    QueueEvents as BullQueueEvents,
+  } from 'bullmq'
+  export class BullMQBaseClass {
+    public connection: BullMQConfig
+    constructor(config: typeof Config)
+    public queue<T, R>(name: string): BullQueue<T, R, string>
+    public worker<T, R, N extends string = string>(
+      name: string,
+      callback: string | Processor<T, R, N>
+    ): BullWorker<T, R, N>
+    public events(name: string): BullQueueEvents
+  }
+  const BullMQClass: BullMQBaseClass
+  export default BullMQClass
 }
